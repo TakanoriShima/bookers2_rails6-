a8b77ed4406b73b5f70acf49aabb94d46e61b7a6
EC2 Default User <ec2-user@ip-172-31-42-211.ap-northeast-1.compute.internal>
2022-09-13 04:50:43 +0000

    一応完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, presence: false, length: { maximum: 50 }
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
 end
